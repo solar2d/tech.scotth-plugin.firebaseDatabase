3a65a289a3d5191823837aa4e98a344c89a8fb53
Scott Harrison <scottrules44@gmail.com>
2022-04-29 00:36:31 -0400

    Update plugin_firebaseDatabase.lua

--- plugins/2014.2264/mac-sim/plugin_firebaseDatabase.lua
+++ plugins/2014.2264/mac-sim/plugin_firebaseDatabase.lua
@@ -3,9 +3,23 @@ local Library = require "CoronaLibrary"
 -- Create stub library for simulator
 local lib = Library:new{ name='plugin.firebaseDatabase', publisherId='tech.scotth' }
 -- Default implementations
+local Library = require "CoronaLibrary"
+
+-- Default implementations
 local function defaultFunction()
-	print( "WARNING: The '" .. lib.name .. "' library is not available on this platform." )
+	native.showAlert( "Warning", "You are using on unsupported plugin, please see updated plugin on solar2dmarketplace.com", { "Ignore", "View Updated Plugin" }, function(event)
+	  if ( event.action == "clicked" ) then
+	      local i = event.index
+	      if ( i == 1 ) then
+		  -- Do nothing; dialog will simply dismiss
+	      elseif ( i == 2 ) then
+		  system.openURL( "https://solar2dmarketplace.com/plugins?FirebaseDatabase_tech-scotth" )
+	      end
+	  end
+	end )
 end
+
+
 
 lib.init = defaultFunction
 lib.get = defaultFunction
